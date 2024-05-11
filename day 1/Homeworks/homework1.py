0ca5607b84d7ae0fac9e731b9522faf493923c2f
barbarerazmadze29 <nataliarzm4@gmail.com>
2024-05-11 19:48:56 +0400

    second commit

---
+++ day 1/Homeworks/homework1.py
@@ -0,0 +1,19 @@
+name1 = "Barbare"
+surname = "Razmadze"
+name2 = "me mqvia"
+name3 = "var"
+name4 = "wlis"
+name5 = "maqvs"
+name7 = "maqvs"
+name6 = "simaglit var"
+name8 = "tvalebi"
+name9 = "metri"
+age = 13
+height = 169.5
+name10 = "lurji"
+name11 = "qera"
+
+print(name2+ " " + name1 + " " + surname + " " + name3 +" " + str(age) + " " + name4 + " " + name5 + " " + name10 + " " + name8 + " " + name3 + " " + name11 + " " + name6 + " " + str(height) + " " + name9)
+
+
+
