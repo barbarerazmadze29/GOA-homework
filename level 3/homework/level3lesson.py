74a682de83a91d22eee868c53a8110801e676067
barbarerazmadze29 <nataliarzm4@gmail.com>
2024-05-20 22:12:18 +0400

    second commit

---
+++ level 3/homework/level3lesson.py
@@ -0,0 +1,66 @@
+#homework N1+N2
+Name = "Barbare"
+Last_name = "Razmadze"
+Age = "13"
+Hobby = "music"
+Country = "georgia"
+
+print(Name + " " + Last_name + ", " + Age + ", " + Hobby + ", " + Country)
+
+
+
+#Homework N3
+print(5+5)
+print(178+45)
+print(73+88)
+print(71+3)
+print(8+0)
+print(2+96)
+print(12+63)
+print(13+14)
+print(0+2)
+print(2+78)
+
+print(4-3)
+print(9-6)
+print(784-24)
+print(93-(-2))
+print(46-(-6-12))
+print(34-12-(+60))
+print(88-34)
+print(52-4-93)
+print(-8-19)
+print(56-56)
+
+print(67*94)
+print(9*(5-4))
+print(43*8)
+print(7*21)
+print(28*9*3)
+print(4*2)
+print(12345*0.5)
+print(0.5*0.5)
+print(34*3.4)
+print(100*0.5+50)
+
+print(5**3)
+print(12**2)
+print(43**9)
+print(8**4)
+print(6**6)
+print((48-39)**4)
+print(2**2**2)
+print((3+3+16)**5)
+print(7**8)
+print((22+33)**(32-28))
+
+print(7//3.5)
+print(6//3)
+print((9+4)//2.8)
+print(0+22//4.3)
+print(78//6)
+print(42+4//5.5)
+print(43//9)
+print(89+3/2//2.4)
+print(9373638//83.1)
+print((40+90-(5+29))//7.2)
