6f8a5ff5b2eeb16451282c3f5547bd35f3a9d52f
barbarerazmadze29 <nataliarzm4@gmail.com>
2024-05-28 22:17:54 +0400

    forth commit

---
+++ level 5/homework/lesson5.py
@@ -0,0 +1,25 @@
+#I created a variable and stored my name in it
+name1 = "Barbare"
+#I created a variable and stored my surname in it
+surname = "Razmadze"
+#I created a variable and stored diffrent words in it
+name2 = "me mqvia"
+name3 = "var"
+name4 = "wlis"
+name5 = "maqvs"
+name7 = "maqvs"
+name6 = "simaglit var"
+name8 = "tvalebi"
+name9 = "metri"
+#I created variable of my age and height.
+age = 13
+height = 169.5
+name10 = "lurji"
+name11 = "qera"
+
+#then I connected diffrent words and information about myself in order, with adding the variables I created earlier, to make a big sentence of a description of myself, using knowlage I got in GOA
+
+print(name2+ " " + name1 + " " + surname + " " + name3 +" " + str(age) + " " + name4 + " " + name5 + " " + name10 + " " + name8 + " " + name3 + " " + name11 + " " + name6 + " " + str(height) + " " + name9)
+
+
+
